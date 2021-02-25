68d6b35c617d98606e51725475fc1506120e4beb
Márcio Saraiva <marciosaraivac2@gmail.com>
2021-02-25 08:29:17 -0300

    Add files via upload

---
+++ constantes.py
@@ -0,0 +1,22 @@
+participantesbbb= {"Arthur":"",
+"Caio":"",
+"Camilla":"",
+"Carla":"",
+"Fiuk":"",
+"Gil":"",
+"João":"",
+"Juliette":"",
+"Lumena":"",
+"Pocah":"",
+"Projota":"",
+"Rodolffo":"",
+"Sarah":"",
+"Thaís":"",
+"Viih":"",
+"Karol": "",
+"Nego Di":"",
+"Arcrebiano" : "",
+"Lucas": "",
+"Kerline": ""
+}
+
